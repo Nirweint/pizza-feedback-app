5b93ce969766adaa15a0849e6bfe2c07ab30464e
Nirweint <nirweint_1@mail.ru>
2022-04-07 09:31:44 +0300

    drag and drop widgets done

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,28 +1,67 @@
-import React from 'react';
+import React, {ReactElement, useState} from 'react';
 import {FeedbackWidget} from "../../components/feedBack/FeedbackWidget";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import {WidgetCard} from "../../components/widgetCard/WidgetCard";
+import {DragDropContext, Draggable, Droppable, DropResult} from "react-beautiful-dnd";
 import {FeedbackProgressWidget} from "../../components/feedbackProgressWidget/FeedbackProgressWidget";
 
+type WidgetType = {
+  id: string;
+  component: ReactElement;
+  title: string;
+}
+
+const widgetsData: WidgetType[] = [{
+  id: '1',
+  component: <FeedbackWidget/>,
+  title: 'Feedback'
+}, {
+  id: '2',
+  component: <FeedbackProgressWidget/>,
+  title: 'Feedback process',
+}];
+
 export const Dashboard = () => {
+
+  const [widgets, setWidgets] = useState<WidgetType[]>(widgetsData)
+
+  const handleOnDragEnd = (result: DropResult) => {
+    if (result.destination) {
+      const items = Array.from(widgets);
+      const [reorderedItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index, 0, reorderedItem);
+      setWidgets(items)
+    }
+  }
 
   return (
     <>
       <Grid container p={2}>
         <Typography variant="h3">Dashboard</Typography>
-        <Grid container>
-          <Grid item marginRight={2}>
-            <WidgetCard title='Feedback'>
-              <FeedbackWidget/>
-            </WidgetCard>
-          </Grid>
-          <Grid item marginRight={2}>
-            <WidgetCard title='Feedback process'>
-              <FeedbackProgressWidget/>
-            </WidgetCard>
-          </Grid>
-        </Grid>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId='widgets'>
+            {(provided): JSX.Element => (
+              <Grid container {...provided.droppableProps} ref={provided.innerRef}>
+                {widgets.map(({id, component, title}, index) => (
+                  <Draggable key={id} draggableId={id} index={index}>
+                    {(provided): JSX.Element => (
+                      <Grid item
+                            marginRight={2} {...provided.draggableProps} {...provided.dragHandleProps}
+                            ref={provided.innerRef}>
+                        <WidgetCard title={title}>
+                          {component}
+                        </WidgetCard>
+                      </Grid>
+                    )}
+                  </Draggable>
+                ))
+                }
+                {provided.placeholder}
+              </Grid>
+            )}
+          </Droppable>
+        </DragDropContext>
       </Grid>
     </>
   );
